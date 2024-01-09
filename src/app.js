a8efff7e0340492972869f320c0198bb3519882a
RazanHarashe <harashe115@gmail.com>
2024-01-09 02:39:49 +0200

    add admin router

--- src/app.js
+++ src/app.js
@@ -1,14 +1,14 @@
 const express = require("express");
 require("./db/mongoose");
 const userRouter = require("./routers/users");
-
+const adminRouter = require("./routers/admin");
 app = express();
 
 const port = process.env.PORT;
 
 app.use(express.json());
 app.use(userRouter);
-
+app.use(adminRouter);
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
