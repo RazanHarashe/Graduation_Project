6ddaf24fa0efd2b19f611399bc29cdf438707005
RazanHarashe <harashe115@gmail.com>
2024-01-09 02:55:36 +0200

    secure the API and Handel the errors middleware

---
+++ middleware/jwt.js
@@ -0,0 +1,24 @@
+const expressJwt = require("express-jwt");
+
+function authJwt() {
+  return expressJwt({
+    secret: process.env.JWT_SECRET,
+    algorithms: ["HS256"],
+    isRevoked: isRevoked,
+  }).unless({
+    path: [
+      { url: /\/users\/confirmEmail(.*)/, methods: ["GET", "OPTIONS"] },
+      "/users/login",
+      "/users/signup",
+      "/users/me",
+    ],
+  });
+}
+async function isRevoked(req, payload, done) {
+  if (!payload.isAdmin) {
+    done(null, true);
+  }
+  done();
+}
+
+module.exports = authJwt;
